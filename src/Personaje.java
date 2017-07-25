5376f6173f46a5f9fc4d2ed58aaa1ce871d7caac
Valentina Tugnarelli <valentinatugnarelli@gmail.com>
2017-07-25 00:43:26 -0300

    canje según tabla

--- src/Personaje.java
+++ src/Personaje.java
@@ -16,11 +16,50 @@ class Personaje {
 		
 		int resultado = puntosDeFuerza + puntos;
 		
-		if ((puntosDisponibles >= puntos) && (resultado <= 18) && (resultado >= 7)) {
+		if ((puntosDisponibles >= puntos) && (resultado >= 6) && (resultado <= 27)) {
 			
-			puntosDisponibles = puntosDisponibles - puntos;
+			if (puntos == -4) {
+				
+				puntosDeFuerza = resultado + 1;
+				
+				puntosDisponibles = puntosDisponibles - puntos;
+				
+			} else if ((puntos >= -2) && (puntos <= 3)) {
+				
+				puntosDeFuerza = resultado;
+				
+				puntosDisponibles = puntosDisponibles - puntos;
 			
-			puntosDeFuerza = resultado;
+			} else if (puntos == 5) {
+				
+				puntosDeFuerza = resultado - 1; 
+				
+				puntosDisponibles = puntosDisponibles - puntos;
+			
+			} else if (puntos == 7) {
+				
+				puntosDeFuerza = resultado - 2;
+				
+				puntosDisponibles = puntosDisponibles - puntos;
+			
+			} else if (puntos == 10) {
+				
+				puntosDeFuerza = resultado - 4;
+				
+				puntosDisponibles = puntosDisponibles - puntos;
+			
+			} else if (puntos == 13) {
+				
+				puntosDeFuerza = resultado - 6;
+				
+				puntosDisponibles = puntosDisponibles - puntos;
+			
+			} else if (puntos == 17) {
+				
+				puntosDeFuerza = resultado - 9;
+				
+				puntosDisponibles = puntosDisponibles - puntos;
+			}
 		}
 	}
 }
